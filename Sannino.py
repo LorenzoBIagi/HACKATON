23d7804d3fb36a44af722c61afc21b8dc185746d
Lorenzo Biagi <lorenzobiagi@student-net-hpx-2438.intern.ethz.ch>
2025-05-09 16:36:43 +0200

    commit

--- Sannino.py
+++ Sannino.py
@@ -1,4 +1,16 @@
-from qiskit import QuantumCircuit
-ciaoooooooooooooo
-ConnectionAbortedErrorcoapc
-cbdaibcvhksd
+from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
+ 
+qubits = QuantumRegister(1)
+clbits = ClassicalRegister(1)
+circuit = QuantumCircuit(qubits, clbits)
+(q0,) = qubits
+(c0,) = clbits
+ 
+circuit.h(q0)
+circuit.measure(q0, c0)
+with circuit.if_test((c0, 1)):
+    circuit.x(q0)
+circuit.measure(q0, c0)
+circuit.draw("mpl")
+ 
+# example output counts: {'0': 1024}
