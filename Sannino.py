c5a3302868081726cdb4bc79bc6770e5205445ff
Lorenzo Biagi <lorenzobiagi@student-net-hci-3653.intern.ethz.ch>
2025-05-10 10:29:39 +0200

    commit

--- Sannino.py
+++ Sannino.py
@@ -1,28 +1,4 @@
-from qiskit import QuantumCircuit, transpile
-from qiskit_aer import AerSimulator
-from qiskit.visualization import plot_histogram
-import matplotlib.pyplot as plt
-%matplotlib inline
-
-# Crea il circuito
-qc = QuantumCircuit(2, 2)
-qc.h(0)
-qc.cx(0, 1)
-qc.measure([0, 1], [0, 1])
-
-
-# Simulatore
-simulator = AerSimulator()
-
-# Transpile per il simulatore
-compiled = transpile(qc, simulator)
-
-# Esegui la simulazione
-job = simulator.run(compiled, shots=1024)
-result = job.result()
-
-# Risultati
-qc.draw('mpl')
-counts = result.get_counts()
-plot_histogram(counts)
-plt.show()
+import numpy as np
+num_dimensions = 1
+mu = 0.10   # Mean vector
+cov_matrix = 0.20  # Covariance matrix 
