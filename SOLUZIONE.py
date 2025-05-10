fc13749db9d30d7a3b106085343cbe470a3c5406
Lorenzo Biagi <lorenzobiagi@student-net-hci-3653.intern.ethz.ch>
2025-05-10 11:20:24 +0200

    commit

--- SOLUZIONE.py
+++ SOLUZIONE.py
@@ -4,20 +4,19 @@ import torch
 
 #Dati 1D
 num_dimensions = 1
-mu = 0.10   # Mean vector
-cov_matrix = 0.20  # Covariance matrix 
+mu = np.array(0.10)   # Mean vector
+cov_matrix = np.array(0.20)  # Covariance matrix 
 
-def function(x,mu,cov_matrix):
-    cov = 0.5 * (cov_matrix + cov_matrix.T)
+cov_sym = 0.5 * (cov_matrix + cov_matrix.T)
+def function(x,mu,cov_sym):
     n = x.shape[0]
-    determinant = np.linalg.det(cov)
+    determinant = np.linalg.det(cov_sym)
     return 1/((2*np.pi)**(n/2) *determinant) * np.exp(-0.5 * (x-mu).T @ np.linalg.inv(cov_matrix) @ (x-mu))
 
 domain_np = [np.linspace(mu[d] - 3*np.sqrt(cov_sym[d,d]), mu[d] + 3*np.sqrt(cov_sym[d,d]), grid_size) for d in range(x.shape[0])]
     
 
-domain=domain()
-t = tn.cross(function=function, domain=domain)
-
+t = tn.cross(function=function, domain=domain_np, max_rank=8)
+print(t)
 
 
--- SanninoMoodys.py
+++ SanninoMoodys.py
@@ -7,21 +7,35 @@ num_dimensions = 2
 mu = np.array([0.10, 0.10])  # Mean vector
 cov_matrix = np.array([[0.20, 0.35],[ 0.16, 0.07]])  # Covariance matrix 
 
+def make_psd(cov):
+    # 1. Simmetrizza
+    cov_sym = 0.5 * (cov + cov.T)
+    
+    # 2. Autovalori e autovettori
+    eigvals, eigvecs = np.linalg.eigh(cov_sym)
+
+    # 3. Azzeramento degli autovalori negativi
+    eigvals_clipped = np.clip(eigvals, a_min=1e-6, a_max=None)  # imposta min=1e-6 per evitare problemi numerici
+
+    # 4. Ricostruzione matrice PSD
+    cov_psd = eigvecs @ np.diag(eigvals_clipped) @ eigvecs.T
+    return cov_psd
+
 
 def generate_tt_pdf(mu, cov, num_qubits_per_dim=10, max_tt_rank=8):
     n_dims = len(mu)
     grid_size = 2 ** num_qubits_per_dim
     #symmtrizziamo la matrice di covarianza
-    cov_sym = 0.5 * (cov + cov.T)
+    cov_psd = make_psd(cov)
 
     # Griglia in ogni dimensione (intervallo centrato attorno a mu)
-    domain_np = [np.linspace(mu[d] - 3*np.sqrt(cov_sym[d,d]), mu[d] + 3*np.sqrt(cov_sym[d,d]), grid_size) for d in range(n_dims)]
+    domain_np = [np.linspace(mu[d] - 3*np.sqrt(cov_psd[d,d]), mu[d] + 3*np.sqrt(cov_psd[d,d]), grid_size) for d in range(n_dims)]
     domain = [torch.tensor(d, dtype=torch.float32) for d in domain_np]
 
     
 
     # Oggetto scipy multivariate normal
-    dist = multivariate_normal(mean=mu, cov=cov_sym)
+    dist = multivariate_normal(mean=mu, cov=cov_psd)
 
     # Funzione PDF vettorializzata
     def pdf_function(*args):
