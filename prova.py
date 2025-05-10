c1252f5cba3256c7cbbd71d5c1480bb8aca6109d
Lorenzo Biagi <lorenzobiagi@student-net-hci-3653.intern.ethz.ch>
2025-05-10 12:57:41 +0200

    commit

---
+++ prova.py
@@ -0,0 +1,22 @@
+import torch
+import tntorch as tn
+
+# 1) Costruisci A: tensore binario d-dimensionale
+d = 5
+A = torch.randn(*([2] * d))  # oppure i tuoi valori
+
+# 2) Avvolgi in un Tensor-Train “grezzo”
+t = tn.Tensor(A)
+
+# 3) Esegui la cross-approximation con funzione identità
+tt = tn.cross(
+    function=lambda x: x,   # identità su ciascuna fibra
+    tensors=[t],            # lista di un solo tensore
+    eps=1e-6,               # tolleranza desiderata
+    rmax=50,                # rank massimo ammesso
+    verbose=True
+)
+
+print("TT cores:", len(tt.cores))
+print("Shapes:", [core.shape for core in tt.cores])
+print(tt)
