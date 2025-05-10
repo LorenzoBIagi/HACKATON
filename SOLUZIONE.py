9e7361c879cf2b432a9b654507237b2ee8f3c86c
Lorenzo Biagi <lorenzobiagi@student-net-hci-3653.intern.ethz.ch>
2025-05-10 11:24:15 +0200

    commit

--- SOLUZIONE.py
+++ SOLUZIONE.py
@@ -10,13 +10,17 @@ cov_matrix = np.array(0.20)  # Covariance matrix
 bond_dimension = 8
 qubits = 10
 cov_sym = 0.5 * (cov_matrix + cov_matrix.T)
-grid_size = 2 ** qubits
-def function(x,mu,cov_sym):
-    n = x.shape[0]
-    determinant = np.linalg.det(cov_sym)
-    return 1/((2*np.pi)**(n/2) *determinant) * np.exp(-0.5 * (x-mu).T @ np.linalg.inv(cov_matrix) @ (x-mu))
+eigvals, eigvecs = np.linalg.eigh(cov_sym)
+eigvals_clipped = np.clip(eigvals, a_min=0, a_max=None)
+cov = eigvecs @ np.diag(eigvals_clipped) @ eigvecs.T
 
-domain_np = [np.linspace(mu[d] - 3*np.sqrt(cov_sym[d,d]), mu[d] + 3*np.sqrt(cov_sym[d,d]), grid_size) for d in range(num_dimensions)]
+grid_size = 2 ** qubits
+def function(x,mu,cov):
+    n = x.shape[0]
+    determinant = np.linalg.det(cov)
+    return 1/((2*np.pi)**(n/2) *determinant) * np.exp(-0.5 * (x-mu).T @ np.linalg.inv(cov) @ (x-mu))
+
+domain_np = [np.linspace(mu[d] - 3*np.sqrt(cov[d,d]), mu[d] + 3*np.sqrt(cov[d,d]), grid_size) for d in range(num_dimensions)]
     
 
 t = tn.cross(function=function, domain=domain_np, max_rank=bond_dimension)
