9e10a838b94bf9cdd9e757b66981086bb19bb778
Lorenzo Biagi <lorenzobiagi@student-net-hci-3653.intern.ethz.ch>
2025-05-11 02:00:04 +0200

    commit

--- FINALE 2D.py
+++ FINALE 2D.py
@@ -12,41 +12,37 @@ from qiskit.circuit.library import UnitaryGate
 
 #PARAMETRI GAUSSIANA
 
-num_dimensions = 1
-mu = 0.10   # Mean vector
-cov_matrix = 0.20  # Covariance matrix 
+num_dimensions = 2
+mu = np.array([0.10, 0.10])  # Mean vector
+cov_matrix = np.array([[0.20, 0.35],[ 0.16, 0.07]])  # Covariance matrix 
 
-#PARAMETRI DISCRETIZZAZIOINE
 
-d = 10                 # qubit
-m = 2**d               # dimensioni discretizzazione
-
-#GAUSSIANA DISCRETA
-
-domain_np = np.linspace(mu - 3*np.sqrt(cov_matrix), mu + 3*np.sqrt(cov_matrix), m)
 def gaussian(x):
-    return (1/(np.sqrt(2*np.pi*cov_matrix)))*np.exp(-0.5*((x-mu)**2)/np.sqrt(cov_matrix))
+    return 1/ (((2 * np.pi) ** (num_dimensions) * np.abs(np.linalg.det(cov_matrix))) ** 0.5) * np.exp(-0.5 * ((x-mu).T @ np.linalg.inv(cov_matrix) @ (x-mu)))
 
+d=10
 
+vectroized_function = [] #vettore vuoto
+for x in np.linspace(mu[0] - 3*np.sqrt(cov_matrix[0,0]), mu[0] + 3*np.sqrt(cov_matrix[0,0]), 2**d):
+    for y in np.linspace(mu[1] - 3*np.sqrt(cov_matrix[1,1]), mu[1] + 3*np.sqrt(cov_matrix[1,1]), 2**d):
+        vectroized_function.append(gaussian(np.array([x,y])))
 
-vec =  np.array([gaussian(x) for x in domain_np])    # vettore probabilità discreta
+vectroized_function = np.array(vectroized_function) # vettore probabilità discreta
 
-shape = (2,)*d         # (2,2,2,2)
-A = vec.reshape(shape) #tensore numpy
+shape = (2,)*(d*num_dimensions)         # (2,2,2,2)
+A = vectroized_function.reshape(shape) #tensore numpy
 T=tn.Tensor(A)      #tensore torch
 
-
-#CREAZIONE TENSOR TRAIN
 
 TTrain = tn.cross(
     function=lambda x: x,   # identità su ciascuna fibra
     tensors=[T],            # lista di un solo tensore               # tolleranza desiderata
-    ranks_tt=8,                 # rank massimo ammesso
+    rmax=8,                 # rank massimo ammesso
 )
 
 
-
 print(TTrain)
+
 
 
 cores_torch = TTrain.cores
