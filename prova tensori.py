cbcbe8d1d923eb10e1d0a58f554f8c46fe2c6814
Lorenzo Biagi <lorenzobiagi@student-net-hci-3653.intern.ethz.ch>
2025-05-11 09:50:40 +0200

    commit

--- prova tensori.py
+++
@@ -1,5 +0,0 @@
-import tntorch as tn
-
-t = tn.ones(64, 64)  # 64 x 64 tensor, filled with ones
-t = t[:, :, None] + 2*t[:, None, :]  # Singleton dimensions, broadcasting, and arithmetics
-print(tn.mean(t))  # Result: 3
